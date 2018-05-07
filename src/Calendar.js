5912a3f4b48aa5d976431090ff44891a0ab765f6
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2018-05-07 13:37:33 -0500

    styling

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import moment from 'moment'
 import { refreshEvents } from './actionCreators'
 
 
@@ -7,6 +8,16 @@ class Calendar extends Component {
     componentDidMount() {
         this.props.dispatch(refreshEvents())
     }
+
+    formatTime(t) {
+        t = moment(t)
+        if(t.isSame(moment(), 'day')) {
+            return t.format('h:mm a')
+        } else {
+            return t.format('dddd h:mm a')
+        }
+    }
+
     render() {
         const events = this.props.events
         if(!events.length) {
@@ -15,17 +26,20 @@ class Calendar extends Component {
         const firstEvent = (
             <div className='event first-event'>
                 <div className='event-name'>{events[0].name}</div>
-                <div className='event-time'>{events[0].time}</div>
+                <div className='event-time'>
+                    {this.formatTime(events[0].time) + ' (' + moment(events[0].time).from(moment()) + ')'}
+                </div>
             </div>
         )
         const nextEvents = events.slice(1, events.length).map(e => (
             <div className='event'>
                 <div className='event-name'>{e.name}</div>
-                <div className='event-time'>{e.time}</div>
+                <div className='event-time'>{this.formatTime(e.time)}</div>
             </div>
         ))
         return (
             <div className='calendar'>
+                <h2>Events</h2>
                 <div className='first-event-container'>{firstEvent}</div>
                 <div className='next-event-list'>{nextEvents}</div>
             </div>
--- src/Todo.js
+++ src/Todo.js
@@ -35,52 +35,63 @@ class Todo extends React.Component {
         const currentTodoList = currentTodo.map(e => (
             <div className='todo-item'>
                 <div className='todo-name'>{e['name']}</div>
-                <button className='todo-button todo-stop'
-                    onClick={event => dispatch(stopTODO(e['id']))}>stop</button>
-                <button className='todo-button todo-finish'
-                    onClick={event => dispatch(finishTODO(e['id']))}>finish</button>
-                <button className='todo-button todo-delete'
-                    onClick={event => dispatch(deleteTODO(e['id']))}>delete</button>
+                <div className='todo-controls'>
+                    <button className='todo-button todo-stop'
+                        onClick={event => dispatch(stopTODO(e['id']))}>stop</button>
+                    <button className='todo-button todo-finish'
+                        onClick={event => dispatch(finishTODO(e['id']))}>finish</button>
+                    <button className='todo-button todo-delete'
+                        onClick={event => dispatch(deleteTODO(e['id']))}>delete</button>
+                </div>
             </div>
         ));
         const nextTodoList = nextTodo.map(e => (
             <div className='todo-item'>
                 <div className='todo-name'>{e['name']}</div>
-                <button className='todo-button todo-start'
-                    onClick={event => dispatch(startTODO(e['id']))}>start</button>
-                <button className='todo-button todo-finish'
-                    onClick={event => dispatch(finishTODO(e['id']))}>finish</button>
-                <button className='todo-button todo-delete'
-                    onClick={event => dispatch(deleteTODO(e['id']))}>delete</button>
+                <div className='todo-controls'>
+                    <button className='todo-button todo-start'
+                        onClick={event => dispatch(startTODO(e['id']))}>start</button>
+                    <button className='todo-button todo-finish'
+                        onClick={event => dispatch(finishTODO(e['id']))}>finish</button>
+                    <button className='todo-button todo-delete'
+                        onClick={event => dispatch(deleteTODO(e['id']))}>delete</button>
+                </div>
             </div>
         ));
         const finishedTodoList = finishedTodo.map(e => (
             <div className='todo-item'>
                 <s className='todo-name'>{e['name']}</s>
-                <button className='todo-button todo-reset'
-                    onClick={event => dispatch(resetTODO(e['id']))}>reset</button>
-                <button className='todo-button todo-delete'
-                    onClick={event => dispatch(deleteTODO(e['id']))}>delete</button>
+                <div className='todo-controls'>
+                    <button className='todo-button todo-reset'
+                        onClick={event => dispatch(resetTODO(e['id']))}>reset</button>
+                    <button className='todo-button todo-delete'
+                        onClick={event => dispatch(deleteTODO(e['id']))}>delete</button>
+                </div>
             </div>
         ));
-        return (
-            <div className='todos'>
+        let currentElem = null
+        if(currentTodoList.length) {
+            currentElem = (
                 <div className='working-on-container'>
-                    <p className='desc-text'>Working on:</p>
+                    <p className='desc-text'>Working On -</p>
                     <div className='todo-list'>{currentTodoList}</div>
                 </div>
+            )
+        }
+        return (
+            <div className='todos'>
+                <h2>TODOS</h2>
+                {currentElem}
                 <div className='new-todo-container'>
-                    <input type='text'
+                    <input type='text' placeholder='New TODO'
                         value={this.state.newTodoName}
                         onChange={this.handleChange}
                         onKeyUp={e => {if (e.key === 'Enter') {this.handleNewTodo(e)}}}/>
-                    <button onClick={this.handleNewTodo}>+</button>
                 </div>
                 <div className='next-container'>
                     <div className='todo-list'>{nextTodoList}</div>
                 </div>
                 <div className='finished-container'>
-                    <p className='desc-text'>Done:</p>
                     <div className='todo-list'>{finishedTodoList}</div>
                 </div>
             </div>
